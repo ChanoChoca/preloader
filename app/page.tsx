0a41652c12788572e87e754a6b9f8638cda6ba77
Juan Ignacio Caprioli <juanicaprioli16@outlook.com>
2025-10-27 20:09:25 -0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Preloader />
-      <main className="bg-[#f3f9d2] w-screen h-svh flex flex-col gap-2 items-center justify-center">
+      <main className="bg-[#f3f9d2] w-screen h-svh flex flex-col gap-2 items-center justify-center text-center">
         <h1>Una visión</h1>
         <h1>Capturada a través de</h1>
         <h1>Animar</h1>
